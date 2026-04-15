766969673d925bd08f6956b0ba0b0f9302ae8f38
Goran Stevanović <stevanovicgoran@proton.me>
2026-04-15 17:07:20 +0200

    refactor: include list of non-available question files, if applicable

--- src/file_utils.py
+++ src/file_utils.py
@@ -14,7 +14,7 @@ def delete_tmp_folder() -> None:
         shutil.rmtree(TEMPORARY_PATH)
 
 
-def check_available_files(folder: str, files: list, kind: str = "pitanja"):
+def check_file_availability(folder: str, files: list, kind: str = "pitanja"):
     """Check if folder contains all provided files."""
     if kind == "pitanja":
         folder_path = QUESTIONS_PATH / folder
@@ -26,25 +26,32 @@ def check_available_files(folder: str, files: list, kind: str = "pitanja"):
         file_name_stripped = int(file.split(".")[0])
         files_in_folder_stripped.append(file_name_stripped)
 
-    files_in_folder_stripped.sort()
+    files_not_available = list(set(files) - set(files_in_folder_stripped))
 
-    return set(files).issubset(files_in_folder_stripped)
+    return files_not_available
 
 
 def check_questions_availability(
     subjects: list[SubjectData],
-) -> tuple[list[SubjectData], list[SubjectData]]:
+) -> tuple[list[SubjectData], list[dict]]:
     """Return tuple containing two lists:
     one with subjects that have all selected questions available,
-    another one with subjects that don't have all selected questions available.
+    another one with subjects that don't have all selected questions available, including missing question numbers.
     """
     question_files_available = []
     question_files_not_available = []
 
     for subject in subjects:
-        if check_available_files(subject["abbreviation"], subject["generated_numbers"]):
-            question_files_available.append(subject)
+        non_available_questions = check_file_availability(
+            subject["abbreviation"], subject["generated_numbers"]
+        )
+
+        if non_available_questions:
+            subject_updated = dict(
+                subject, **{"non_available_questions": non_available_questions}
+            )
+            question_files_not_available.append(subject_updated)
         else:
-            question_files_not_available.append(subject)
+            question_files_available.append(subject)
 
     return question_files_available, question_files_not_available
