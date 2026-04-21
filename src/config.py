359c197720947cdc46b0dd6b73a46f23df522976
Goran Stevanović <stevanovicgoran@proton.me>
2026-04-21 15:04:58 +0200

    fix: folder path for answer documents

--- src/config.py
+++ src/config.py
@@ -21,7 +21,7 @@ GENERATED_NUMBERS_RANGE = "F10:F29"
 # Folder paths
 BASE_PATH = Path("baza")
 QUESTIONS_PATH = BASE_PATH / "pitanja"
-ANSWERS_PATH = BASE_PATH / "pitanja"
+ANSWERS_PATH = BASE_PATH / "odgovori"
 TEMPLATES_PATH = BASE_PATH / "predlosci"
 OUTPUT_PATH = Path("generisani-testovi")
 TEMPORARY_PATH = Path("tmp")
