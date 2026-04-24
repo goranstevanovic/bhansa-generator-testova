6c51a516e7150603f16f0bfd911f539d37c14f59
Goran Stevanović <stevanovicgoran@proton.me>
2026-04-24 10:48:17 +0200

    test: patch file path of cover template for test answers

--- tests/test_writer.py
+++ tests/test_writer.py
@@ -178,7 +178,7 @@ class TestCreateCoverPage:
 # Tests for generate_document_for_subject()
 class TestGenerateDocumentForSubject:
     @patch("writer.OUTPUT_PATH", SAMPLE_OUTPUT_PATH)
-    @patch("writer.COVER_TEMPLATE", SAMPLE_COVER_TEMPLATE)
+    @patch("writer.COVER_TEMPLATE_ANSWERS", SAMPLE_COVER_TEMPLATE_ANSWERS)
     @patch("writer.TEMPORARY_PATH", SAMPLE_TEMPORARY_PATH)
     @patch("writer.TEMPLATE_TITLE_STRING", SAMPLE_TEMPLATE_TITLE_STRING)
     @patch("writer.TEMPLATE_ABBREVIATION_STRING", SAMPLE_TEMPLATE_ABBREVIATION_STRING)
@@ -231,7 +231,7 @@ class TestGenerateDocumentForSubject:
         assert sample_questions["pitanje9"] not in full_text
 
     @patch("writer.OUTPUT_PATH", SAMPLE_OUTPUT_PATH)
-    @patch("writer.COVER_TEMPLATE", SAMPLE_COVER_TEMPLATE)
+    @patch("writer.COVER_TEMPLATE_ANSWERS", SAMPLE_COVER_TEMPLATE_ANSWERS)
     @patch("writer.TEMPORARY_PATH", SAMPLE_TEMPORARY_PATH)
     @patch("writer.TEMPLATE_TITLE_STRING", SAMPLE_TEMPLATE_TITLE_STRING)
     @patch("writer.TEMPLATE_ABBREVIATION_STRING", SAMPLE_TEMPLATE_ABBREVIATION_STRING)
@@ -281,7 +281,7 @@ class TestDocumentsForAllSubjects:
         assert all(res.exists() for res in results)
 
     @patch("writer.OUTPUT_PATH", SAMPLE_OUTPUT_PATH)
-    @patch("writer.COVER_TEMPLATE", SAMPLE_COVER_TEMPLATE)
+    @patch("writer.COVER_TEMPLATE_ANSWERS", SAMPLE_COVER_TEMPLATE_ANSWERS)
     @patch("writer.TEMPORARY_PATH", SAMPLE_TEMPORARY_PATH)
     @patch("writer.TEMPLATE_TITLE_STRING", SAMPLE_TEMPLATE_TITLE_STRING)
     @patch("writer.TEMPLATE_ABBREVIATION_STRING", SAMPLE_TEMPLATE_ABBREVIATION_STRING)
